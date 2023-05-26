e4a5c5635c136b22b5e77c193a9698e89e9fbfa1
yeongwoo <jyy0223@naver.com>
2023-05-26 13:03:41 +0900

    [add] const color

---
+++ lib/const/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// 주색상
+const PRIMARY_COLOR = Color(0XFF2571E1);
+// 메인 글자 색상
+const BODY_TEXT_COLOR = Color(0xFF030303);
+// 서브 텍스트 색상
+const SUB_BODY_TEXT_COLOR = Color(0XFF98989A);
