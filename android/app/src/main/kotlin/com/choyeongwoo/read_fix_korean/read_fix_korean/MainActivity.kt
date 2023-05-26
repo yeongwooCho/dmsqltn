b87c5fee61425fba3e43c241208c254c1786f1bc
yeongwoo <jyy0223@naver.com>
2023-05-26 12:56:34 +0900

    [setting] project init

---
+++ android/app/src/main/kotlin/com/choyeongwoo/read_fix_korean/read_fix_korean/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.choyeongwoo.read_fix_korean.read_fix_korean
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:read_fix_korean/screen/root_screen.dart';
+
+void main() {
+  runApp(const _App());
+}
+
+class _App extends StatelessWidget {
+  const _App({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: backgroundColor,
+        sliderTheme: SliderThemeData(
+          thumbColor: primaryColor,
+          activeTrackColor: primaryColor,
+          inactiveTrackColor: secondaryColor,
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: backgroundColor,
+          selectedItemColor: primaryColor,
+          unselectedItemColor: secondaryColor,
+        ),
+      ),
+      home: const RootScreen(),
+    );
+}
