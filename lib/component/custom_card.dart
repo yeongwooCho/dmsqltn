b3515d0d5132629b2993fdc92cb59827a7adb66a
yeongwoo <jyy0223@naver.com>
2023-05-27 18:56:38 +0900

    [chore] 비교정답 삭제에 따른 디자인 변경

--- lib/component/custom_card.dart
+++ lib/component/custom_card.dart
@@ -4,22 +4,15 @@ import 'package:read_fix_korean/const/colors.dart';
 class CustomCard extends StatelessWidget {
   final bool isCorrect;
   final String title;
-  final String errorText;
 
   const CustomCard({
     Key? key,
     required this.isCorrect,
     required this.title,
-    required this.errorText,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final miniTextStyle = TextStyle(
-      fontSize: 14.0,
-      fontWeight: FontWeight.bold,
-    );
-
     return Container(
       decoration: BoxDecoration(
         color: isCorrect ? TRUE_BOX_COLOR : FALSE_BOX_COLOR,
@@ -28,19 +21,21 @@ class CustomCard extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(
           horizontal: 20.0,
-          vertical: 14.0,
+          vertical: 20.0,
         ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
               title,
-              style: miniTextStyle,
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            Text(
-              errorText,
-              style: miniTextStyle.copyWith(color: Colors.grey),
-            ),
+            isCorrect
+                ? const Icon(Icons.check_circle)
+                : const Icon(Icons.circle_outlined)
           ],
         ),
       ),
--- lib/screen/scan_screen.dart
+++ lib/screen/scan_screen.dart
@@ -16,56 +16,48 @@ class ScanScreen extends StatefulWidget {
 }
 
 class _ScanScreenState extends State<ScanScreen> {
-  // File? _image;
   bool _scanning = false;
   String _extractText = '';
   XFile? _pickedImage;
 
-  // Future<void> getImage(ImageSource imageSource) async {
-  //   final image = await ImagePicker().pickImage(source: imageSource);
-  //
-  //   setState(() {
-  //     _image = File(image!.path); // 가져온 이미지를 _image에 저장
-  //   });
-  //
-  //   // getOcrText();
-  // }
-  //
-  // Future<void> getOcrText() async {
-  //   //---- dynamic add Tessdata (Android)---- ▼
-  //   // https://github.com/tesseract-ocr/tessdata/raw/main/dan_frak.traineddata
-  //
-  //   const String langName = 'kor';
-  //   HttpClient httpClient = new HttpClient();
-  //
-  //   HttpClientRequest request = await httpClient.getUrl(Uri.parse(
-  //       'https://github.com/tesseract-ocr/tessdata/raw/main/${langName}.traineddata'));
-  //
-  //   HttpClientResponse response = await request.close();
-  //   Uint8List bytes =await consolidateHttpClientResponseBytes(response);
-  //   String dir = await FlutterTesseractOcr.getTessdataPath();
-  //
-  //   print('$dir/${langName}.traineddata');
-  //   File file = File('$dir/${langName}.traineddata');
-  //   await file.writeAsBytes(bytes);
-  // }
+  Future<void> getOcr() async {
+    //---- dynamic add Tessdata (Android)---- ▼
+    // https://github.com/tesseract-ocr/tessdata/raw/main/dan_frak.traineddata
 
-  // 이미지를 보여주는 위젯
-  // Widget showImage() {
-  //   return SizedBox(
-  //       width: MediaQuery.of(context).size.width,
-  //       height: MediaQuery.of(context).size.width,
-  //       child: Image.file(File(_image!.path))
-  //   );
-  // }
+    const String langName = 'kor';
+    HttpClient httpClient = new HttpClient();
+
+    HttpClientRequest request = await httpClient.getUrl(Uri.parse(
+        'https://github.com/tesseract-ocr/tessdata/raw/main/${langName}.traineddata'));
+
+    HttpClientResponse response = await request.close();
+    Uint8List bytes = await consolidateHttpClientResponseBytes(response);
+    String dir = await FlutterTesseractOcr.getTessdataPath();
+
+    print('$dir/${langName}.traineddata');
+    File file = File('$dir/${langName}.traineddata');
+    await file.writeAsBytes(bytes);
+  }
 
   @override
   Widget build(BuildContext context) {
-    final textStyle = TextStyle(
-      fontSize: 20.0,
-      fontWeight: FontWeight.bold,
-    );
+    return _scanning
+        ? const CircularProgressIndicator()
+        : getMainColumnWidget();
+  }
 
+  void onPressOpenCamera() async {
+    setState(() {
+      _scanning = true;
+    });
+    _pickedImage = await ImagePicker().pickImage(source: ImageSource.camera);
+    _extractText = await FlutterTesseractOcr.extractText(_pickedImage!.path);
+    setState(() {
+      _scanning = false;
+    });
+  }
+
+  Widget getMainColumnWidget() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -83,7 +75,10 @@ class _ScanScreenState extends State<ScanScreen> {
                     width: double.infinity,
                     child: Text(
                       '추천 정답',
-                      style: textStyle,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                   const SizedBox(height: 16.0),
@@ -93,74 +88,26 @@ class _ScanScreenState extends State<ScanScreen> {
                       CustomCard(
                         isCorrect: false,
                         title: '1. 사람들에게 사진 바람이나',
-                        errorText: 'Error(123)',
+                      ),
+                      const SizedBox(height: 8.0),
+                      CustomCard(
+                        isCorrect: true,
+                        title: '1. 사람들에게 사진 바람이나',
                       ),
                       const SizedBox(height: 8.0),
                       CustomCard(
                         isCorrect: false,
                         title: '1. 사람들에게 사진 바람이나',
-                        errorText: 'Error(123)',
                       ),
                       const SizedBox(height: 8.0),
                       CustomCard(
                         isCorrect: false,
                         title: '1. 사람들에게 사진 바람이나',
-                        errorText: 'Error(123)',
                       ),
                       const SizedBox(height: 8.0),
                       CustomCard(
                         isCorrect: false,
                         title: '1. 사람들에게 사진 바람이나',
-                        errorText: 'Error(123)',
-                      ),
-                      const SizedBox(height: 8.0),
-                      CustomCard(
-                        isCorrect: false,
-                        title: '1. 사람들에게 사진 바람이나',
-                        errorText: 'Error(123)',
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 32.0),
-                  SizedBox(
-                    width: double.infinity,
-                    child: Text(
-                      '비교 정답',
-                      style: textStyle,
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      CustomCard(
-                        isCorrect: false,
-                        title: '1. 사람들에게 사진 바람이나',
-                        errorText: 'Error(123)',
-                      ),
-                      const SizedBox(height: 8.0),
-                      CustomCard(
-                        isCorrect: false,
-                        title: '1. 사람들에게 사진 바람이나',
-                        errorText: 'Error(123)',
-                      ),
-                      const SizedBox(height: 8.0),
-                      CustomCard(
-                        isCorrect: false,
-                        title: '1. 사람들에게 사진 바람이나',
-                        errorText: 'Error(123)',
-                      ),
-                      const SizedBox(height: 8.0),
-                      CustomCard(
-                        isCorrect: false,
-                        title: '1. 사람들에게 사진 바람이나',
-                        errorText: 'Error(123)',
-                      ),
-                      const SizedBox(height: 8.0),
-                      CustomCard(
-                        isCorrect: false,
-                        title: '1. 사람들에게 사진 바람이나',
-                        errorText: 'Error(123)',
                       ),
                     ],
                   ),
@@ -169,42 +116,8 @@ class _ScanScreenState extends State<ScanScreen> {
             ),
           ),
         ),
-
         ElevatedButton(
-          // getImage(ImageSource.camera);
-
-          // onPressed: () async {
-          //   setState(() {
-          //     _scanning = true;
-          //   });
-          //
-          //   // _image = getImage(ImageSource.camera);
-          //   // _image = await getImage(ImageSource.camera)
-          //
-          //   _pickedImage =
-          //   await ImagePicker().pickImage(source: ImageSource.camera);
-          //
-          //   FlutterTesseractOcr.extractText(imagePath)
-          //
-          //   _extractText =
-          //   await FlutterTesseractOcr.extractText(_pickedImage!.path);
-          //   setState(() {
-          //     _scanning = false;
-          //
-          //   });
-          // },
-          onPressed: () async {
-            setState(() {
-              _scanning = true;
-            });
-            _pickedImage =
-            await ImagePicker().pickImage(source: ImageSource.camera);
-            _extractText =
-            await FlutterTesseractOcr.extractText(_pickedImage!.path);
-            setState(() {
-              _scanning = false;
-            });
-          },
+          onPressed: onPressOpenCamera,
           style: ElevatedButton.styleFrom(
             backgroundColor: PRIMARY_COLOR,
             minimumSize: const Size(100, 60),
@@ -213,11 +126,11 @@ class _ScanScreenState extends State<ScanScreen> {
               fontWeight: FontWeight.bold,
             ),
           ),
-          child: Row(
+          child: const Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Icon(Icons.photo_camera),
-              const SizedBox(width: 8.0),
+              SizedBox(width: 8.0),
               Text('Open Camera'),
             ],
           ),
